49ca784e620c6ec9e2dcfb0c5f0173519002f3f2
pauguz1 <elsantana5@gmail.com>
2026-05-04 18:53:12 -0600

    se agrego la estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './App.css'
 import LandingPageComponent from './pages/LandingPage/LandingPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,17 +4,23 @@ import { useRef } from "react";
 import '../../global.css';
 import './LandingPage.css';
 
-//importamos los componentes
+//importamos iconos
+import iconDev from '../../assets/icons/iconDev.svg'
+import iconBook from '../../assets/icons/iconBook.svg'
+import iconMe from '../../assets/icons/iconMe.svg'
+import iconCamera from '../../assets/icons/iconCamera.svg'
+import iconGYM from '../../assets/icons/iconGYM.svg'
 
-//importamos el logo
 
 
 const LandingPageComponent = ()=>{
 
     //referencias para el foco de cada seccion
-    const valorRef = useRef(null);
-    const serviciosRef = useRef(null);
-    const contactoRef = useRef(null);
+    const devRef = useRef(null);
+    const booksRef = useRef(null);
+    const meRef = useRef(null);
+    const fotoRef = useRef(null);
+    const gymRef = useRef(null);
 
     //este metodo da foco a la referencia que le pasemos como parametro
   const darFoco = (info) => {
@@ -52,33 +58,65 @@ const asesor = `
 
     return (
         <section className="LandingPage">
+
+            {/* navbar para la parte superior*/}
             <nav className='navbar-LandingPage'>
-                <a href='/'>
-                    Paul Santana
-                </a>
+                <a href='/'>Paul Santana</a>
                 <div>
-                    <h4 onClick={() => darFoco(valorRef)} >Valor</h4>
-                    <h4 onClick={() => darFoco(serviciosRef)} >Servicios</h4>
-                    <h4 onClick={() => darFoco(contactoRef)} >Contacto</h4>
+                    <h4>Entrendo</h4>
                 </div>
             </nav>
 
-            <section className='seccion seccion-index'>
+            {/* navbar para las opciones */}
+            <div className="navbar-options">
+              <div className="navbar-options-item" onClick={() => darFoco(devRef)} >
+                <img src={iconDev} alt="icon developer" />
+                <span>DEV</span>
+              </div>
+              <div className="navbar-options-item" onClick={() => darFoco(booksRef)} >
+                <img src={iconBook} alt="icon book" />
+                <span>Books</span>
+              </div>
+              <div className="navbar-options-item" onClick={() => darFoco(meRef)} >
+                <img src={iconMe} alt="icon me" />
+                <span>Me</span>
+              </div>
+              <div className="navbar-options-item" onClick={() => darFoco(fotoRef)} >
+                <img src={iconCamera} alt="icon camera" />
+                <span>Foto</span>
+              </div>
+              <div className="navbar-options-item" onClick={() => darFoco(gymRef)} >
+                <img src={iconGYM} alt="icon GYM" />
+                <span>GYM</span>
+              </div>
+            </div>
+
+
+            {/* seccion Developer*/}
+            <section ref={devRef}  className='seccion seccion-index'>
                 
             </section>
 
-            <section ref={valorRef} className='seccion'>
+            {/* seccion Books*/}
+            <section ref={booksRef} className='seccion'>
                 
 
             </section>
 
-            <section ref={serviciosRef} className='seccion'>
+
+            {/* seccion Me*/}
+            <section ref={meRef} className='seccion'>
                 
 
             </section>
 
+            {/* seccion Foto*/}
+            <section ref={fotoRef} className='seccion'>
+                
+            </section>
 
-            <section ref={contactoRef} className='seccion'>
+            {/* seccion GYM*/}
+            <section ref={gymRef} className='seccion'>
                 
             </section>
 
