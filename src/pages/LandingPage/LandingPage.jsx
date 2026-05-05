851f9b6986f7bf6d557bf16a82156a2f84bd3715
pauguz1 <elsantana5@gmail.com>
2026-05-04 19:28:29 -0600

    se agrego la imagen de fondo y los titulos iniciales, se corrigio el box sizing del navbar options

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -94,7 +94,8 @@ const asesor = `
 
             {/* seccion Developer*/}
             <section ref={devRef}  className='seccion seccion-index'>
-                
+                <h1>ey its me</h1>
+                <h3>La vida es muy corta para hacer una sola cosa</h3>
             </section>
 
             {/* seccion Books*/}
